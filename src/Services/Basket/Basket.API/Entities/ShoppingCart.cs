3855ac309f0f1014d3450e95edaaeca4c32cfd4c
Gustavo Avila <gusavilasilva@gmail.com>
2021-05-31 22:31:06 -0300

    initializing Basket API microservice

---
+++ src/Services/Basket/Basket.API/Entities/ShoppingCart.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Basket.API.Entities
+{
+    public class ShoppingCart
+    {
+        public string UserName { get; set; }
+        public List<ShoppingCartItem> Items { get; set; } = new List<ShoppingCartItem>();
+
+        public decimal TotalPrice
+        {
+            get
+            {
+                decimal totalPrice = 0;
+                foreach(var item in Items)
+                {
+                    totalPrice += item.Price;
+                }
+
+                return totalPrice;
+            }
+        }
+    }
+}
