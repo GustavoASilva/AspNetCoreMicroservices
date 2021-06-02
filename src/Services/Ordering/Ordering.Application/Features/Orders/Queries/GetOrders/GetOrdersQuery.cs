43717c41ebca0ab788bf9492736ac592677dd383
Gustavo Avila <gusavilasilva@gmail.com>
2021-06-02 00:21:32 -0300

    setting up CQRS pattern and GetOrdersQuery/Handler

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Queries/GetOrders/GetOrdersQuery.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Features.Orders.Queries.GetOrders
+{
+    public class GetOrdersQuery : IRequest<List<GetOrdersDto>>
+    {
+        public string UserName { get; set; }
+
+        public GetOrdersQuery(string userName)
+        {
+            UserName = userName;
+        }
+
+    }
+}
---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Queries/GetOrders/GetOrdersQueryHandler.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using MediatR;
+using Ordering.Application.Contracts.Persistence;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Features.Orders.Queries.GetOrders
+{
+    public class GetOrdersQueryHandler : IRequestHandler<GetOrdersQuery, List<GetOrdersDto>>
+    {
+        private readonly IOrderRepository _orderRepository;
+        private readonly IMapper _mapper;
+
+        public GetOrdersQueryHandler(IOrderRepository orderRepository, IMapper mapper)
+        {
+            _orderRepository = orderRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<List<GetOrdersDto>> Handle(GetOrdersQuery request, CancellationToken cancellationToken)
+        {
+            var orders = await _orderRepository.GetOrdersByUserName(request.UserName);
+            return _mapper.Map<List<GetOrdersDto>>(orders);
+        }
+    }
+}
